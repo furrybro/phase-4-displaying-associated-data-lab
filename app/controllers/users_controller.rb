c6008d7d57ad8e0807a3b3c4578ae571fe2146db
jeena park <jeenapark15@gmail.com>
2022-07-05 17:53:38 -0400

    commit tree 702d704028f1512a125402dfe94c5f0dbec86513

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        render json: user.to_json(only: [:id, :username, :city], include: :items)
+    end
+
 end
